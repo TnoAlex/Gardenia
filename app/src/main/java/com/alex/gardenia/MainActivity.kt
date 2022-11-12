8686c84525ca54a1ae87a2ed133da00db75be6a1
23103 <2310350113@qq.com>
2022-11-12 15:46:36 +0800

    第一次提交

---
+++ app/src/main/java/com/alex/gardenia/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alex.gardenia
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        println("hello")
+
+    }
+}
--- app/src/main/java/com/alex/gardenia/activity/LoginActivity.kt
+++ app/src/main/java/com/alex/gardenia/activity/LoginActivity.kt
@@ -1,9 +1,9 @@
 package com.alex.gardenia.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import com.alex.gardenia.R
 
 class LoginActivity : AppCompatActivity() {
@@ -11,10 +11,15 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val singUpLabel = findViewById<TextView>(R.id.sing_up_inner)
-        singUpLabel.setOnClickListener{
-            val singUpIntent = Intent(LoginActivity@this,SignupActivity::class.java)
-            startActivity(singUpIntent)
+
+        val editTextUserName = findViewById<EditText>(R.id.user_name)
+        val editTextPassword = findViewById<EditText>(R.id.user_paswd)
+        val loginButton :Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            println(editTextUserName.text)
+            println(editTextPassword.text)
         }
+
+
     }
 }
