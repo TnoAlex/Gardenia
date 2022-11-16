d4391fd983fedd3b085db3644eb7aba2a5707c69
Nicholas_Alex <alex@njust.edu.cn>
2022-11-16 20:08:31 +0800

    用户端分析页面布局完成

--- app/src/main/java/com/alex/gardenia/activity/AnalyticsActivity.kt
+++ app/src/main/java/com/alex/gardenia/activity/AnalyticsActivity.kt
@@ -1,12 +1,12 @@
-package com.alex.gardenia
+package com.alex.gardenia.activity
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alex.gardenia.R
 
-
-class MainActivity : AppCompatActivity() {
+class AnalyticsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        println("hello")
+        setContentView(R.layout.activity_analytics)
     }
 }
