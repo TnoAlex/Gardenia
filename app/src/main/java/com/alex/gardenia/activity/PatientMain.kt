437c3c3e3b541cd64e3ba17d07379b191f03d5d0
Nicholas_Alex <alex@njust.edu.cn>
2022-11-17 22:07:14 +0800

    用户端主页面布局完成

---
+++ app/src/main/java/com/alex/gardenia/activity/PatientMain.kt
@@ -0,0 +1,12 @@
+package com.alex.gardenia.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alex.gardenia.R
+
+class PatientMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_main)
+    }
+}
---
+++ app/src/main/java/com/alex/gardenia/utils/DensityUtil.kt
@@ -0,0 +1,43 @@
+package com.alex.gardenia.utils
+
+import android.content.Context
+import android.util.TypedValue
+
+
+object DensityUtil {
+
+    /**
+     * dp转px
+     */
+    fun dp2px(context: Context, dpVal: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dpVal, context.resources.displayMetrics
+        ).toInt()
+    }
+
+    /**
+     * sp转px
+     */
+    fun sp2px(context: Context, spVal: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            spVal, context.resources.displayMetrics
+        ).toInt()
+    }
+
+    /**
+     * px转dp
+     */
+    fun px2dp(context: Context, pxVal: Float): Float {
+        val scale: Float = context.resources.displayMetrics.density
+        return pxVal / scale
+    }
+
+    /**
+     * px转sp
+     */
+    fun px2sp(context: Context, pxVal: Float): Float {
+        return pxVal / context.resources.displayMetrics.scaledDensity
+    }
+}
